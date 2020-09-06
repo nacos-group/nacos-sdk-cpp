fe6116b26e13b02b783fab2037156dd12d382d4a
TTTTTAAAAAKKKKEEEENNNN <48080812+TTTTTAAAAAKKKKEEEENNNN@users.noreply.github.com>
2020-09-06 11:34:36 +0800

    Create ServerSelector.h

---
+++ include/server/ServerSelector.h
@@ -0,0 +1,19 @@
+#ifndef NACOS_SDK_CPP_SERVERSELECTOR_H
+#define NACOS_SDK_CPP_SERVERSELECTOR_H
+
+#include "NacosServerInfo.h"
+#include <list>
+
+/*
+* ServerSelector
+* Author: Liu, Hanyu
+* Selector interface, to select one server from the select list
+* User can implement different selecting strategies (e.g.: Weighed, Random, Least used) by implementing select() method
+*/
+class ServerSelector {
+public:
+    virtual NacosServerInfo select(std::list<NacosServerInfo> &serverList) = 0;
+    virtual ~ServerSelector() {};
+};
+
+#endif //NACOS_SDK_CPP_SERVERSELECTOR_H
