400146736379c519a8b2d37055f46c2cfec495e8
TTTTTAAAAAKKKKEEEENNNN <48080812+TTTTTAAAAAKKKKEEEENNNN@users.noreply.github.com>
2020-09-10 22:20:02 +0800

    amend for pr14 UUID gen

--- include/utils/UuidUtils.h
+++ include/utils/UuidUtils.h
@@ -2,6 +2,7 @@
 #define __UUID_UTILS_H_
 #include "NacosString.h"
 
+#define UUID_LEN_BYTES 16
 class UuidUtils
 {
 public:
--- src/utils/UuidUtils.cpp
+++ src/utils/UuidUtils.cpp
@@ -1,26 +1,28 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
 
 #include "utils/UuidUtils.h"
 
 NacosString UuidUtils::generateUuid()
 {
 	int fd=0;
-	char buf;
+	char random_num[UUID_LEN_BYTES];
+	char str_buffer[33];
 
-	fd=open("/dev/urandom",O_RDONLY);
+	fd = open("/dev/urandom",O_RDONLY);
 
-	int k = 0;
-	std::string uuid(16, ' ');
-	while(k < 16){
-		read(fd, &buf, 1);
-		if(std::isalpha(buf) || std::isalnum(buf)){
-			uuid[k] = buf;
-			k++;
-		}
-	}
+	size_t bytes_read = 0;
+	while (bytes_read < UUID_LEN_BYTES)//an UUID has 128 bits(16 bytes), so we need to 16 bytes from the urandom
+    {
+        bytes_read += read(fd, &random_num[bytes_read], UUID_LEN_BYTES - bytes_read);
+    }
+
+    int32_t *val_ptr = (int32_t*)random_num;
+	sprintf(str_buffer, "%08X%08X%08X%08X", val_ptr[0], val_ptr[1], val_ptr[2], val_ptr[3]);
 
 	close(fd);
+	NacosString uuid = str_buffer;
 	return uuid;
 }
