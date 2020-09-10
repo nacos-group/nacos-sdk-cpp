84483dfc635f0612624ea7c0fb63de9232f3c04f
yzz <1445277435@qq.com>
2020-09-10 16:49:35 +0800

    fix uuid generator

--- src/utils/UuidUtils.cpp
+++ src/utils/UuidUtils.cpp
@@ -7,18 +7,20 @@
 NacosString UuidUtils::generateUuid()
 {
 	int fd=0;
-	char buf[16];
+	char buf;
 
 	fd=open("/dev/urandom",O_RDONLY);
 
 	int k = 0;
+	std::string uuid(16, ' ');
 	while(k < 16){
-		read(fd, buf+k, 1);
-		if(std::isalpha(*(buf+k)) || std::isalnum(*(buf+k))){
+		read(fd, &buf, 1);
+		if(std::isalpha(buf) || std::isalnum(buf)){
+			uuid[k] = buf;
 			k++;
 		}
 	}
 
 	close(fd);
-	return std::string(buf);
+	return uuid;
 }
