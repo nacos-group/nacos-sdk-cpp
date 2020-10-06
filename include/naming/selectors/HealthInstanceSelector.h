a25bb8c512bba0b54e35f1c89379bfe9f453ca8f
TTTTTAAAAAKKKKEEEENNNN <48080812+TTTTTAAAAAKKKKEEEENNNN@users.noreply.github.com>
2020-10-06 22:57:20 +0800

    healthy instance selector

---
+++ include/naming/selectors/HealthInstanceSelector.h
@@ -0,0 +1,24 @@
+#ifndef __HEALTH_INST_SELECTOR_H_
+#define __HEALTH_INST_SELECTOR_H_
+
+#include <list>
+#include "naming/selectors/Selector.h"
+#include "Debug.h"
+
+class HealthInstanceSelector : public Selector<Instance>{
+private:
+public:
+    std::list<Instance> select(const std::list<Instance> &instancesToSelect){
+        std::list<Instance> result;
+        for (std::list<Instance>::const_iterator it = instancesToSelect.begin();
+            it != instancesToSelect.end(); it++) {
+            if (it->healthy) {
+                result.push_back(*it);
+            }
+        }
+
+        return result;
+    }
+};
+
+#endif
