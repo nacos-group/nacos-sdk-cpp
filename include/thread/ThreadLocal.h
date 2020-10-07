9f9162e8aec19fd27a77301e41bf51df8b4a569a
TTTTTAAAAAKKKKEEEENNNN <48080812+TTTTTAAAAAKKKKEEEENNNN@users.noreply.github.com>
2020-10-07 23:20:43 +0800

    + threadlocal (similar as Java)
    I have to admit that memory management is soooooooooo much more complicated than in Java

---
+++ include/thread/ThreadLocal.h
@@ -0,0 +1,65 @@
+#ifndef __THREAD_LOCAL_H_
+#define __THREAD_LOCAL_H_
+
+#include <pthread.h>
+
+template <typename T>
+class ThreadLocal;
+template <typename T>
+struct ObjectWrapper {
+    T wrappedObject;
+    ThreadLocal<T> *threadLocalObj;
+};
+
+template <typename T>
+class ThreadLocal{
+private:
+    pthread_key_t pthreadKey;
+    static void destroyer(void *param) {
+        ObjectWrapper<T> *wrapper = reinterpret_cast< ObjectWrapper<T> *>(param);
+        wrapper->threadLocalObj->onDestroy(wrapper->wrappedObject);
+        delete wrapper;
+    }
+
+    ObjectWrapper<T> *getWrapper() const {
+        ObjectWrapper<T> *wrapper = reinterpret_cast< ObjectWrapper<T> *>(pthread_getspecific(pthreadKey));
+        return wrapper;
+    }
+public:
+    ThreadLocal() {
+        /* init the curl session */
+        pthread_key_create(&pthreadKey, destroyer);
+    }
+
+    void set(T value) {
+        ObjectWrapper<T> *wrapper = getWrapper();
+        if (wrapper == NULL) {
+            wrapper = new ObjectWrapper<T>;
+            wrapper->threadLocalObj = this;
+            onCreate(wrapper->wrappedObject);
+            pthread_setspecific(pthreadKey, reinterpret_cast<void *>(wrapper));
+        }
+        wrapper->wrappedObject = value;
+    }
+
+    T get() const {
+        ObjectWrapper<T> *wrapper = getWrapper();
+        return wrapper->wrappedObject;
+    }
+
+    virtual ~ThreadLocal() {
+        pthread_key_delete(pthreadKey);
+    }
+
+    virtual void onCreate(T value) {
+        //do nothing by default;
+        //!!!!!shall NOT access anything other than VALUE!!!!!
+    }
+
+    virtual void onDestroy(T value) {
+        //do nothing by default;
+        //!!!!!shall NOT access anything other than VALUE!!!!!
+    }
+};
+
+#endif
