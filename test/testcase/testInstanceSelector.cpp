aed7ef09a29493d7244f9e25aa7d8708744b6b89
TTTTTAAAAAKKKKEEEENNNN <48080812+TTTTTAAAAAKKKKEEEENNNN@users.noreply.github.com>
2020-10-07 19:24:57 +0800

    randomByWeight instance selector and testcase

--- test/testcase/testInstanceSelector.cpp
+++ test/testcase/testInstanceSelector.cpp
@@ -4,6 +4,7 @@
 #include <list>
 #include "factory/NacosServiceFactory.h"
 #include "naming/Instance.h"
+#include "naming/selectors/RandomByWeightSelector.h"
 #include "naming/selectors/HealthInstanceSelector.h"
 #include "naming/selectors/RandomSelector.h"
 #include "Constants.h"
@@ -32,7 +33,20 @@ bool testInstanceSelectors() {
     list<Instance> res;
     HealthInstanceSelector healthInstanceSelector;
     RandomSelector randomSelector;
+
+    Instance instance;
+    instance.clusterName = "DefaultCluster";
+    instance.ip = "127.0.0.1";
+    instance.port = 2333;
+    instance.instanceId = "1";
+    instance.ephemeral = true;
+
     try {
+        for (int i = 0; i < 10; i++) {
+            NacosString serviceName = "TestNamingService0";
+            instance.port = 2000 + i;
+            namingSvc->registerInstance(serviceName, instance);
+        }
         res = namingSvc->getInstanceWithPredicate("TestNamingService0", &randomSelector);
     }
     catch (NacosException e) {
@@ -43,6 +57,55 @@ bool testInstanceSelectors() {
     for (list<Instance>::const_iterator it = res.begin();
          it != res.end(); it++) {
         cout << "service:" << it->toString() << endl;
+    }
+
+    return true;
+}
+
+bool testRandomByWeightSelector()
+{
+
+    cout << "in function testRandomByWeightSelector" << endl;
+    Properties configProps;
+    configProps[PropertyKeyConst::SERVER_ADDR] = "127.0.0.1";
+    configProps[PropertyKeyConst::TCP_NAMING_POLL_INTERVAL] = "3000";
+
+    NacosServiceFactory *factory = new NacosServiceFactory(configProps);
+    ResourceGuard <NacosServiceFactory> _guardFactory(factory);
+    NamingService *namingSvc = factory->CreateNamingService();
+    ResourceGuard <NamingService> _guardService(namingSvc);
+
+    Instance instance;
+    instance.clusterName = "DefaultCluster";
+    instance.ip = "127.0.0.1";
+    instance.port = 2333;
+    instance.instanceId = "1";
+    instance.ephemeral = true;
+    RandomByWeightSelector randomByWeightSelector;
+
+    try {
+        for (int i = 0; i < 10; i++) {
+            NacosString serviceName = "TestNamingService0";
+            instance.port = 2000 + i;
+            instance.weight = i + 1;
+            namingSvc->registerInstance(serviceName, instance);
+        }
+    }
+    catch (NacosException e) {
+        cout << "encounter exception while registering service instance, raison:" << e.what() << endl;
+        return false;
+    }
+
+    getchar();
+
+    for (int i = 0; i < 20; i++)
+    {
+        list<Instance> res = namingSvc->getInstanceWithPredicate("TestNamingService0", &randomByWeightSelector);
+
+        for (list<Instance>::const_iterator it = res.begin();
+             it != res.end(); it++) {
+            cout << "service:" << it->toString() << endl;
+        }
     }
 
     return true;
