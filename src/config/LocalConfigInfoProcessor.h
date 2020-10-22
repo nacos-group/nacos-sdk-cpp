9a0254a69ab4ff6c10958446580fd380d486962d
TTTTTAAAAAKKKKEEEENNNN <48080812+TTTTTAAAAAKKKKEEEENNNN@users.noreply.github.com>
2020-10-22 22:05:03 +0800

    CacheInfo dont use singleton mode

--- src/config/LocalConfigInfoProcessor.h
+++ src/config/LocalConfigInfoProcessor.h
@@ -3,6 +3,7 @@
 
 #include "NacosString.h"
 #include "NacosExceptions.h"
+#include "src/config/AppConfigManager.h"
 
 /**
  * Local Disaster Recovery Directory Tool
@@ -12,41 +13,42 @@
 
 namespace nacos{
 class LocalConfigInfoProcessor {
+private:
+    AppConfigManager *_appCfgMgr;
+    NacosString LOCAL_SNAPSHOT_PATH;
 public:
-    static NacosString getFailover(const NacosString &serverName, const NacosString &dataId, const NacosString &group,
+
+    LocalConfigInfoProcessor(AppConfigManager *appConfigManager);
+
+    NacosString getFailover(const NacosString &serverName, const NacosString &dataId, const NacosString &group,
                                    const NacosString &tenant);
 
     /**
      * Accuire local cache content, returns NULLSTR when the file does not exist or an exception is thrown
      * 获取本地缓存文件内容。NULL表示没有本地文件或抛出异常。
      */
-    static NacosString getSnapshot(const NacosString &name, const NacosString &dataId, const NacosString &group,
+    NacosString getSnapshot(const NacosString &name, const NacosString &dataId, const NacosString &group,
                                    const NacosString &tenant);
 
-    static NacosString readFile(const NacosString &file) throw(IOException);
+    NacosString readFile(const NacosString &file) throw(IOException);
 
-    static void saveSnapshot(const NacosString &envName, const NacosString &dataId, const NacosString &group,
+    void saveSnapshot(const NacosString &envName, const NacosString &dataId, const NacosString &group,
                              const NacosString &tenant, const NacosString &config);
 
     /**
      * Purge all cached files in snapshot directory
      * 清除snapshot目录下所有缓存文件。
      */
-    static void cleanAllSnapshot();
+    void cleanAllSnapshot();
 
-    static void cleanEnvSnapshot(const NacosString &envName);
+    void cleanEnvSnapshot(const NacosString &envName);
 
-    static NacosString
+    NacosString
     getFailoverFile(const NacosString &serverName, const NacosString &dataId, const NacosString &group,
                     const NacosString &tenant);
 
-    static NacosString getSnapshotFile(const NacosString &envName, const NacosString &dataId, const NacosString &group,
+    NacosString getSnapshotFile(const NacosString &envName, const NacosString &dataId, const NacosString &group,
                                        const NacosString &tenant);
-
-    static NacosString LOCAL_FILEROOT_PATH;
-    static NacosString LOCAL_SNAPSHOT_PATH;
-
-    static void init();
 };
 }//namespace nacos
 
--- src/init/init.cpp
+++ src/init/init.cpp
@@ -1,5 +1,4 @@
 #include "init.h"
-#include "Debug.h"
 #include "src/http/HTTPCli.h"
 #include "src/config/LocalConfigInfoProcessor.h"
 #include "src/config/SnapShotSwitch.h"
@@ -23,12 +22,9 @@ void Init::doInit() {
     inited = true;
     Debug::set_debug_level(DEBUG);
     HTTPCli::HTTP_GLOBAL_INIT();
-    LocalConfigInfoProcessor::init();
     UtilAndComs::Init();
     RandomUtils::Init();
     UuidUtils::Init();
-    log_debug("LOCAL_FILEROOT_PATH = %s\n", LocalConfigInfoProcessor::LOCAL_FILEROOT_PATH.c_str());
-    log_debug("LOCAL_SNAPSHOT_PATH = %s\n", LocalConfigInfoProcessor::LOCAL_SNAPSHOT_PATH.c_str());
 }
 
 void Init::doDeinit() {
