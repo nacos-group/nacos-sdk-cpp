1b0af42e167578474c5e2557d9309a949d37be85
TTTTTAAAAAKKKKEEEENNNN <48080812+TTTTTAAAAAKKKKEEEENNNN@users.noreply.github.com>
2020-12-06 13:50:11 +0800

    getCurrentTime refactor

---
+++ src/utils/TimeUtils.h
@@ -0,0 +1,14 @@
+#ifndef __TIME_UTILS_H_
+#define __TIME_UTILS_H_
+
+#include <stdint.h>
+#include <sys/time.h>
+
+namespace nacos{
+class TimeUtils {
+public:
+    static int64_t getCurrentTimeInMs();
+};
+}//namespace nacos
+
+#endif
