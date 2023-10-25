a0f9d9ca245adfca4d3fb39847821c7552935c4d
chengteng.ct <chengteng.ct@alibaba-inc.com>
2023-10-25 11:47:05 +0800

    fix 编码风格统一

--- src/constant/PropertyKeyConst.cpp
+++ src/constant/PropertyKeyConst.cpp
@@ -9,7 +9,7 @@ const NacosString PropertyKeyConst::ENDPOINT_PORT = "endpointPort";
 
 const NacosString PropertyKeyConst::NAMESPACE = "namespace";
 
-const NacosString PropertyKeyConst::ENDPOINT_QUERY_PARAMS = "endpoint.query.params";
+const NacosString PropertyKeyConst::ENDPOINT_QUERY_PARAMS = "endpointQueryParams";
 
 const NacosString PropertyKeyConst::ACCESS_KEY = "accessKey";
 
